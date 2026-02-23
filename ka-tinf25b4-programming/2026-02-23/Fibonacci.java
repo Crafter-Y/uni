949f95670279d968e4f8720487e27844b3ee1d5c
Crafter-Y <mrcrafter.yt1@gmail.com>
2026-02-23 22:19:18 +0100

    finish programming notes

--- ka-tinf25b4-programming/2026-02-23/Fibonacci.java
+++ ka-tinf25b4-programming/2026-02-23/Fibonacci.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Fibonacci {
     private static int fib(int n) {
-       if (n == 1 || n == 2) return 1; 
-       return fib(n-1)+fib(n-2);
+       if (n == 1 || n == 2) return 1;
+
+       return fib(n-1) + fib(n-2);
     }
 
     @SuppressWarnings("resource")
@@ -13,8 +14,12 @@ public class Fibonacci {
         int n;
         while (true) {
             System.out.print("Wie viele Fibonacci-Zahlen berechnen? ");
-            n = scanner.nextInt();
-            if (n >= 1) break;
+            try {
+                String next = scanner.nextLine();
+                n = Integer.parseInt(next);
+                if (n >= 1) break;
+            } catch (Exception ignored) {}
+            System.out.println("Die Eingegebene Zahl ist nicht gültig.");
         }
 
         for (int i = 1; i <= n; i++) {
